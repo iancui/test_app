bfd32d38c15e4555c260dcdfbe8949e08648c543
iancui <232747379@qq.com>
2014-11-01 19:06:18 +0800

    Finish user edit,update,index, and destroy actions

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,8 @@
 class UsersController < ApplicationController
+  before_action :signed_in_user, only: [:index,:edit, :update,:destroy]
+  before_action :correct_user,   only: [:edit, :update]
+  before_action :admin_user,     only: :destroy
+
 	def show
 			@user = User.find(params[:id])
 		end
@@ -18,9 +22,51 @@ class UsersController < ApplicationController
 	    end
  	end
 
- 	private 
- 	def user_params
- 		params.require(:user).permit(:name, :email, :password,
-                                   :password_confirmation)
+ 	def edit
+ 		
  	end
+
+  def index
+    @users = User.paginate(page: params[:page])
+  end
+
+ 	def update
+ 		@user = User.find(params[:id])
+    	if @user.update_attributes(user_params)
+      	flash[:success] = "Profile updated"
+        # puts @user
+        sign_in @user
+        redirect_to @user
+    	else
+     		render 'edit'
+    	end
+ 	end
+  def destroy
+    User.find(params[:id]).destroy
+    flash[:success] = "User destroyed"
+    redirect_to users_url
+  end
+
+ 	private 
+ 		def user_params
+ 			params.require(:user).permit(:name, :email, :password,
+                                   :password_confirmation)
+ 		end
+
+    def signed_in_user
+      # redirect_to signin_path,notice:"Please sign in." unless signed_in?
+      unless signed_in?
+        store_location
+        redirect_to signin_url, notice: "Please sign in."
+      end
+    end
+
+    def correct_user
+      @user = User.find(params[:id])
+      redirect_to(root_path) unless current_user?(@user)
+    end
+
+    def admin_user
+      redirect_to(root_path) unless current_user.admin?
+    end
 end
--- spec/factories.rb
+++ spec/factories.rb
@@ -1,8 +1,13 @@
 FactoryGirl.define do
   factory :user do
-    name     "Iancui"
-    email    "iancui@tom.com"
+    # name     "Iancui"
+    # email    "iancui@tom.com"
+    sequence(:name) {|n| "Person #{n}"}
+    sequence(:email) {|n| "pserson_#{n}@example.com"}
     password "foobar"
     password_confirmation "foobar"
+  end
+  factory :admin do
+  	admin true
   end
 end
